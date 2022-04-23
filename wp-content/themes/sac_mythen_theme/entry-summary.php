61986650f2e1e0b09ecd2ae7791409011fba85e8
Matthias Ulrich <post@ulrich.digital>
2022-04-23 08:38:04 +0200

    Delete wp-content/themes/sac_mythen_theme directory

--- wp-content/themes/sac_mythen_theme/entry-summary.php
+++
@@ -1,9 +0,0 @@
-<section class="entry-summary">
-	
-	<?php if(get_post_type()=="touren"): 
-		
-		echo "Touren";
-	endif; ?>
-	<?php the_excerpt(); ?>
-	<?php if( is_search() ) { ?><div class="entry-links"><?php wp_link_pages(); ?></div><?php } ?>
-</section>
