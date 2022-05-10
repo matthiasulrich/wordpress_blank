199a203253f655f4b1a23c5e2a18dc4c8d89d450
Matthias Ulrich <post@ulrich.digital>
2022-05-10 16:50:56 +0200

    Update entry-meta.php

--- wp-content/themes/blank_theme/entry-meta.php
+++ wp-content/themes/blank_theme/entry-meta.php
@@ -1,5 +1,8 @@
 <section class="entry-meta">
 	<span class="author vcard"><?php the_author_posts_link(); ?></span>
+	<!-- oder 
+	<span class="author vcard"> echo get_the_author_meta('display_name');</span>
+	-->
 	<span class="meta-sep"> | </span>
 	<span class="entry-date"><?php the_time( get_option( 'date_format' ) ); ?></span>
 </section>
